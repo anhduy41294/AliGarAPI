de11b3112c18611b4bd5ee7fae50a0d245ff578b
Anh Duy <anhduy41294@gmail.com>
2016-05-16 18:47:56 +0700

    Update New Project

---
+++ AliGarAPI/AliGarAPI/Controllers/DeviceAPIController.cs
@@ -0,0 +1,38 @@
+﻿using AliGarAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AliGarAPI.Controllers
+{
+    public class DeviceAPIController : ApiController
+    {
+        #region Helper
+        public HttpResponseMessage CreateResponse<T>(HttpStatusCode statusCode, T data)
+        {
+            return Request.CreateResponse(statusCode, data);
+        }
+
+        public HttpResponseMessage CreateResponse(HttpStatusCode statusCode)
+        {
+            return Request.CreateResponse(statusCode);
+        }
+
+        #endregion
+
+        [HttpGet]
+        [Route("api/device/all")]
+        public HttpResponseMessage GetAll()
+        {
+            using (QLAliGarEntities ctx = new QLAliGarEntities())
+            {
+                var list = ctx.Devices.ToList();
+
+                return CreateResponse(HttpStatusCode.OK, list);
+            }
+        }
+    }
+}
---
+++ AliGarAPI/AliGarAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AliGarAPI.WebApiApplication" Language="C#" %>
