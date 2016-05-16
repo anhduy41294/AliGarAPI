4f90d8ef16b0471e2a5e55bd05d95612a5e33eeb
Anh Duy <anhduy41294@gmail.com>
2016-05-16 12:35:00 +0700

    Create Reopisitory

---
+++ AliGarAPI/Controllers/DeviceAPIController.cs
@@ -0,0 +1,39 @@
+﻿using AliGarAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AliGarAPI.Controllers
+{
+
+    public class DeviceAPIController : ApiController
+    {
+        #region Helper
+        public HttpResponseMessage CreateResponse<T>(HttpStatusCode statusCode, T data)
+        {
+            return Request.CreateResponse(statusCode, data);
+        }
+
+        public HttpResponseMessage CreateResponse(HttpStatusCode statusCode)
+        {
+            return Request.CreateResponse(statusCode);
+        }
+
+        #endregion
+
+        [HttpGet]
+        [Route("api/device/all")]
+        public HttpResponseMessage GetAll()
+        {
+            using (AliGarDBEntities ctx = new AliGarDBEntities())
+            {
+                var list = ctx.Devices.ToList();
+
+                return CreateResponse(HttpStatusCode.OK, list);
+            }
+        }
+    }
+}
