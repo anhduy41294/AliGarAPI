5d02239bb6cdc810cfefefb33241eb69bad514f8
Anh Duy <anhduy41294@gmail.com>
2016-05-16 20:52:39 +0700

    Update Device Controller

--- AliGarAPI/Controllers/DeviceAPIController.cs
+++ AliGarAPI/Controllers/DeviceAPIController.cs
@@ -34,5 +34,95 @@ namespace AliGarAPI.Controllers
                 return CreateResponse(HttpStatusCode.OK, list);
             }
         }
-    }
+
+        [HttpGet]
+        [Route("api/device/detail")]
+        public HttpResponseMessage Detail([FromUri]decimal id)
+        {
+            using (QLAliGarEntities ctx = new QLAliGarEntities())
+            {
+                var device = ctx.Devices.Where(r => r.IdDevice == id).FirstOrDefault();
+
+                if (device == null)
+                {
+                    return CreateResponse(HttpStatusCode.BadRequest);
+                }
+
+                return CreateResponse(HttpStatusCode.OK, device);
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/device/delete")]
+        public HttpResponseMessage Delete([FromUri]decimal id)
+        {
+            using (QLAliGarEntities ctx = new QLAliGarEntities())
+            {
+                var device = ctx.Devices.Where(r => r.IdDevice == id).FirstOrDefault();
+
+                if (device == null)
+                {
+                    return CreateResponse(HttpStatusCode.BadRequest);
+                }
+
+                try
+                {
+                    ctx.Devices.Remove(device);
+                    int affected = ctx.SaveChanges();
+                    return CreateResponse(HttpStatusCode.OK, affected);
+                }
+                catch (Exception e)
+                {
+                    return CreateResponse(HttpStatusCode.Conflict);
+                }
+            }
+        }
+        [HttpPost]
+        [Route("api/device/add")]
+        public HttpResponseMessage Add([FromBody]Device newDevice)
+        {
+            using (QLAliGarEntities ctx = new QLAliGarEntities())
+            {
+                try
+                {
+                    ctx.Devices.Add(newDevice);
+                    int affected = ctx.SaveChanges();
+                    return CreateResponse(HttpStatusCode.OK, affected);
+                }
+                catch (Exception e)
+                {
+                    return CreateResponse(HttpStatusCode.Conflict);
+                }
+
+            }
+        }
+
+        [HttpPost]
+        [Route("api/device/update")]
+        public HttpResponseMessage Update([FromBody]Device updatedDevice)
+        {
+            using (QLAliGarEntities ctx = new QLAliGarEntities())
+            {
+                var device = ctx.Devices.Where(sv => sv.IdDevice == updatedDevice.IdDevice).FirstOrDefault();
+
+                if (device == null)
+                {
+                    return CreateResponse(HttpStatusCode.BadRequest);
+                }
+                try
+                {
+                    device.DeviceName = updatedDevice.DeviceName;
+                    device.DeviceStatus = updatedDevice.DeviceStatus;
+
+                    int affected = ctx.SaveChanges();
+
+                    return CreateResponse(HttpStatusCode.OK, affected);
+                }
+                catch (Exception e)
+                {
+                    return CreateResponse(HttpStatusCode.Conflict);
+                }
+            }
+        }
+    }   
 }
